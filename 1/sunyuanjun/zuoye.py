ff8a9462d8ac4878ff013afa2e1bdce63407a45e
sunyuanjun <031251100@163.com>
2018-09-03 10:44:09 +0800

    增加add功能

--- 1/sunyuanjun/zuoye.py
+++ 1/sunyuanjun/zuoye.py
@@ -1,4 +1,4 @@
-# encoding:utf-8
+﻿# encoding:utf-8
 user_info = {
     'zhangsan':{'age':20,'phone_no':88888888,},
     'xiaoming':{'age':18,'phone_no':12345678},
@@ -28,6 +28,12 @@ def list_user():
     for k,v in user_info.items():
         print(k,v,sep='\n')
 
+def add_user(username,age,phone_no):
+    if username in user_info.keys():
+        print(f"User {username} is exist.")
+    else:
+        user_info[username] = {'age':age,'phone_no':phone_no}
+
 
 while True:
     action = input('''Input "delete" to delete user
@@ -35,23 +41,30 @@ while True:
     Input "find" to select user info
     Input "list" to show all users
     Input "exit" to quit the program
+    Input "add" to add user
     Please input the operator:''')
     if action == 'delete':
-        user = input('�������û���:')
+        user = input('input username:')
         delete_user(user)
         continue
     elif action == 'update':
-        user = input('�������û���:')
-        age = int(input('����������:'))
-        phone_no = int(input('��������ϵ�绰:'))
+        user = input('input username:')
+        age = int(input('input age:'))
+        phone_no = int(input('input phone number:'))
         update_user(user,age,phone_no)
         continue
     elif action == 'find':
-        user = input('�������û���:')
+        user = input('input username:')
         find_user(user)
         continue
     elif action == 'list':
         list_user()
+        continue
+    elif action == 'add':
+        user = input('input username:')
+        age = int(input('input age:'))
+        phone_no = int(input('input phone number:'))
+        add_user(user,age,phone_no)
         continue
     elif action == 'exit':
         print('Quit the program.')
