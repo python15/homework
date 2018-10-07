a21ad09fb7b19289ff8dc09038e437d6c5aac818
movsb <anhbk@qq.com>
2018-10-07 16:29:44 +0800

    forth homework

---
+++ 4/chenqingmei/ComplexSort.py
@@ -0,0 +1,21 @@
+#!/bin/python3.6
+
+string = 'HelpSorting179062345'
+
+def complexSort(src: str):
+    oddNum = []
+    evenNum = []
+    lowerLetter = []
+    upperLetter = []
+    for i in range(len(src)):
+        if src[i].isdigit():
+            oddNum.append(src[i]) if (int.from_bytes(src[i].encode(), 'big')) % 2 else evenNum.append(src[i])
+        elif src[i].isalpha():
+            lowerLetter.append(src[i]) if src[i].islower() else upperLetter.append(src[i])
+    oddNum = sorted(oddNum)
+    evenNum = sorted(evenNum)
+    lowerLetter = sorted(lowerLetter)
+    upperLetter = sorted(upperLetter)
+    return ''.join(oddNum)+''.join(evenNum)+''.join(lowerLetter)+''.join(upperLetter)
+
+print(complexSort(string))
