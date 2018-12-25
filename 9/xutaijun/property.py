079843c1347d23ce9ce858d650a37057a5174a78
xutaijun <842845009@qq.com>
2018-12-25 04:06:05 -0500

    commit ted

---
+++ 9/xutaijun/property.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+
+class Property:
+    def __init__(self,fget,fset=None):
+        self.fget = fget
+        self.fset = fset
+    def __get__(self,instance,owner):
+        if instance is not None:
+            print(instance)
+            print(owner)
+            return self.fget(instance)
+        return self
+    def __set__(self, instance, value):
+        print(",",self.fset)
+        self.fset(instance,value)
+    def setter(self,fn):
+        self.fset = fn
+        return self
+class A:
+    def __init__(self,data):
+        self._data = data
+    @Property #data = Property(data)
+    def data(self):
+        return self._data
+
+    @data.setter # data = data.setter(data)
+    def data(self,value):
+        self._data = value
+
+a = A(100)
+print(a.data)
+
+print("="*59)
+a.data = 200
