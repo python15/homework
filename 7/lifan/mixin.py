8aa94c2522154ce2531f9f880d48722312826fb1
lifan <lifan@ehaoyao.com>
2018-12-28 13:13:36 +0800

    resubmit

--- 7/lifan/mixin.py
+++ 7/lifan/mixin.py
@@ -9,28 +9,23 @@ import json
 
 class MiXin:
     def ToDict(self):
-        with open('dict.json', 'w+') as f:
-            json.dump(self.stInfo, f)
+        return json.dumps(self.__dict__)
 
 
 class StInfo:
-    def __init__(self):
-        self.stInfo = {}
+    def __init__(self,name,age,discipline):
+        self.name = name
+        self.age = age
+        self.discipline = discipline
 
     def __repr__(self):
-        return str(self.stInfo)
-
-    def __setitem__(self, key, value):
-        self.stInfo[key] = value
-
-
-
+        return str(self.__dict__)
 
 class DictMiXin(MiXin, StInfo): pass
 
 
-st = DictMiXin()
-st['name'],st['age'],st['discipline'] = 'zs',16,'IT'
-st.ToDict()
+st = DictMiXin('zs',16,'IT')
+print(st)                       #{'name': 'zs', 'age': 16, 'discipline': 'IT'}
+print(st.ToDict())              #{"name": "zs", "age": 16, "discipline": "IT"}
 
-# todict 是把属性转化成字典
+
