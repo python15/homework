5e9a359cfff2fc8adc2d2ed4951df2b0a04a4d16
kousong <41051385+kousong@users.noreply.github.com>
2019-06-15 19:44:56 +0800

    Update homework.py
    在notebook里带有中文，直接复制过来造成乱码。已改正

--- 3/kousong/homework.py
+++ 3/kousong/homework.py
@@ -1,13 +1,13 @@
-#third homework:���밢��������
+#third homework:翻译阿拉伯数字
 num=(input("give me a four-number please:"))
-data={"0":"��","1":"Ҽ","2":"��","3":"��","4":"��","5":"��","6":"½","7":"��","8":"��","9":"��"}
-lstt=[]#���ж�����
-lst=[]#������ʵ����
-#�����ֳ��ַ�д���б�
+data={"0":"零","1":"壹","2":"贰","3":"叁","4":"肆","5":"伍","6":"陆","7":"柒","8":"捌","9":"玖"}
+lstt=[]#待判断数据
+lst=[]#最终真实数据
+#将数分成字符写入列表
 for i in str(num):
     lstt.append(i)
 
-#������Ϊ0�����
+#首数字为0的情况
 while lstt[0]=="0":
     lenth=len(lstt)
     if lenth>1:
@@ -17,55 +17,55 @@ while lstt[0]=="0":
     
 
 
-#���������б��γɣ���ʼ���� 
+#最终数据列表形成，开始分析
 
 lst=lstt
 lenth=len(lst)
 
-#��λ
+#一位数
 if lenth<2:
-    print(data[lst[0]]+" "+"Ԫ")
+    print(data[lst[0]]+" "+"元")
 
-#��λ��
+#两位数
 elif lenth<3:
     if lst[1]=="0":
-        print(data[lst[0]]+"ʮ"+" "+"Ԫ")
+        print(data[lst[0]]+"十"+" "+"元")
     else:
-        print(data[lst[0]]+"ʮ"+data[lst[1]]+" "+"Ԫ")
+        print(data[lst[0]]+"十"+data[lst[1]]+" "+"元")
 
-#��λ��
+#三位数
 elif lenth<4:
     if lst[1]=="0":
         if lst[2]=="0":
-            print(data[lst[0]]+"��"+" "+"Ԫ")
+            print(data[lst[0]]+"佰"+" "+"元")
         else:
-            print(data[lst[0]]+"����"+data[lst[2]]+" "+"Ԫ")
+            print(data[lst[0]]+"佰零"+data[lst[2]]+" "+"元")
     elif lst[2]=="0":
-        print(data[lst[0]]+"��"+data[lst[1]]+"ʮ"+" "+"Ԫ")
+        print(data[lst[0]]+"佰"+data[lst[1]]+"十"+" "+"元")
     else:
-        print(data[lst[0]]+"��"+data[lst[1]]+"ʮ"+data[lst[2]]+" "+"Ԫ")
-#��λ��
+        print(data[lst[0]]+"佰"+data[lst[1]]+"十"+data[lst[2]]+" "+"元")
+#四位数
 else:
     if lst[1]=="0":
         if lst[2]=="0":
             if lst[3]=="0":
-                print(data[lst[0]]+"Ǫ"+" "+"Ԫ")
+                print(data[lst[0]]+"仟"+"元")
             else:
-                print(data[lst[0]]+"Ǫ��"+data[lst[3]]+" "+"Ԫ")
+                print(data[lst[0]]+"仟零"+data[lst[3]]+"元")
         elif lst[3]=="0":
-            print(data[lst[0]]+"Ǫ��"+data[lst[2]]+"ʮ"+" "+"Ԫ")
+            print(data[lst[0]]+"仟零"+data[lst[2]]+"十"+" "+"元")
         else:
-            print(data[lst[0]]+"Ǫ��"+data[lst[2]]+"ʮ"+data[lst[3]]+" "+"Ԫ")
+            print(data[lst[0]]+"仟零"+data[lst[2]]+"十"+data[lst[3]]+" "+"元")
     elif lst[2]=="0":
         if lst[3]=="0":
-            print(data[lst[0]]+"Ǫ"+data[lst[1]]+"��"+" "+"Ԫ")
+            print(data[lst[0]]+"仟"+data[lst[1]]+"佰"+" "+"元")
         else:
-            print(data[lst[0]]+"Ǫ"+data[lst[1]]+"����"+data[lst[3]]+" "+"Ԫ")
+            print(data[lst[0]]+"仟"+data[lst[1]]+"佰零"+data[lst[3]]+" "+"元")
     else:
         if lst[3]=="0":
-            print(data[lst[0]]+"Ǫ"+data[lst[1]]+"��"+data[lst[2]]+"ʮ"+" "+"Ԫ")
+            print(data[lst[0]]+"仟"+data[lst[1]]+"佰"+data[lst[2]]+"十"+" "+"元")
         else:
-            print(data[lst[0]]+"Ǫ"+data[lst[1]]+"��"+data[lst[2]]+"ʮ"+data[lst[3]]+" "+"Ԫ")
+            print(data[lst[0]]+"仟"+data[lst[1]]+"佰"+data[lst[2]]+"十"+data[lst[3]]+" "+"元")
 
 
 # 我这边看到的是乱码，可以的话 私聊我下
