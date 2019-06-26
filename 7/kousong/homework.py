fec4e6bdfb0066c436735267eff9504bd05ce8ad
kousong <1036893518@qq.com>
2019-06-26 14:46:09 +0800

    homework

---
+++ 7/kousong/homework.py
@@ -0,0 +1,28 @@
+#seventh week
+import pickle
+import json
+
+
+class Mixin:
+    def __init__(self,d):
+        self.d = d
+
+    def to_dict(self):
+        d=self.__dict__
+
+        pick=pickle.dumps(d)
+        p=pickle.loads(pick)
+        print('pickle          ',pick,p)
+
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ')
+
+        jas=json.dumps(d)
+        j=json.loads(jas)
+        print('jason           ',jas,j)
+
+class A(Mixin):
+    pass
+
+
+a=A(True)
+a.to_dict()
