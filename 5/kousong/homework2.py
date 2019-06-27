5443e7b7cd40d5dead65a1faa2a30ca2509ad1e2
magedu-python <37565803+magedu-python@users.noreply.github.com>
2019-06-27 08:50:14 +0800

    Update homework2.py
    第5周作业批注2

--- 5/kousong/homework2.py
+++ 5/kousong/homework2.py
@@ -1,8 +1,6 @@
 #fifth week2
 d={}
-while True:
-    n=int(input('give me a number'))
-    def cache(fn):
+def cache(fn):
         def _logger(n):
             
             dic=d.keys()
@@ -13,9 +11,11 @@ while True:
                 d[n] = fn(n)
                 return (d[n])       
         return _logger
-    @cache
-    def fib(n):
+ @cache
+ def fib(n):
         if n<3:
             return (n)
         return(fib(n-2)+fib(n-1))
+while True:
+    n=int(input('give me a number'))
     print(fib(n))
